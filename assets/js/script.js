5c19a1a400ea329098a319dcac8db78eadc83073
mostafa hosseini <mostafa.hosseini.db@gmail.com>
2019-07-07 11:19:50 +0430

    arabic V

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,36 @@
 $(document).ready(function () {
     ScrollReveal({ mobile: false });
-    ScrollReveal().reveal('.intro h2', { delay: 200, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
-    ScrollReveal().reveal('.intro p', { delay: 300, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
-    ScrollReveal().reveal('.feature', { delay: 400, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
-    ScrollReveal().reveal('.feature header', { delay: 500, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
-    ScrollReveal().reveal('.feature_details header h3', { delay: 200, distance: '10px', origin: 'right', easing: 'ease-in-out' });
-    ScrollReveal().reveal('.feature_details header p', { delay: 400, distance: '10px', origin: 'right', easing: 'ease-in-out' });
-    ScrollReveal().reveal('.feature_details header img', { delay: 400, distance: '10px', origin: 'top', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.intro h2', { delay: 100, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.intro p', { delay: 200, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.feature', { delay: 300, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.feature header', { delay: 400, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.feature_details header h3', { delay: 100, distance: '10px', origin: 'right', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.feature_details header p', { delay: 300, distance: '10px', origin: 'right', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.feature_details header img', { delay: 300, distance: '10px', origin: 'top', easing: 'ease-in-out' });
 
     for (var i = 1; i <= 6; i++) {
-        ScrollReveal().reveal('.feature_details li:nth-child(' + i + ')', { delay: i * 150, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
-        ScrollReveal().reveal('.feature_details li:nth-child(' + i + ') h3', { delay: 300, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
-        ScrollReveal().reveal('.feature_details li:nth-child(' + i + ') p', { delay: 350, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
+        ScrollReveal().reveal('.feature_details li:nth-child(' + i + ')', { delay: i * 100, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
+        ScrollReveal().reveal('.feature_details li:nth-child(' + i + ') h3', { delay: 200, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
+        ScrollReveal().reveal('.feature_details li:nth-child(' + i + ') p', { delay: 250, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
     }
-    ScrollReveal().reveal('.section2 .img', { delay: 200, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
-    ScrollReveal().reveal('.section2 .img img', { delay: 600, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
-    ScrollReveal().reveal('.section2 .content', { delay: 1000, distance: '10px', origin: 'right', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.section2 .img', { delay: 100, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.section2 .img img', { delay: 500, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.section2 .content', { delay: 800, distance: '10px', origin: 'right', easing: 'ease-in-out' });
 
 
-    ScrollReveal().reveal('.section3 .img', { delay: 200, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
-    ScrollReveal().reveal('.section3 .img img', { delay: 600, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
-    ScrollReveal().reveal('.section3 .content', { delay: 1000, distance: '10px', origin: 'right', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.section3 .img', { delay: 100, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.section3 .img img', { delay: 500, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.section3 .content', { delay: 800, distance: '10px', origin: 'right', easing: 'ease-in-out' });
 
 
-    ScrollReveal().reveal('.section4 .img', { delay: 200, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
-    ScrollReveal().reveal('.section4 .img img', { delay: 600, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
-    ScrollReveal().reveal('.section4 .content', { delay: 1000, distance: '10px', origin: 'right', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.section4 .img', { delay: 100, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.section4 .img img', { delay: 500, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.section4 .content', { delay: 800, distance: '10px', origin: 'right', easing: 'ease-in-out' });
 
 
-    ScrollReveal().reveal('.section5 h3', { delay: 200, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
-    ScrollReveal().reveal('.section5 p', { delay: 300, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
-    ScrollReveal().reveal('.section5 img', { delay: 600, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.section5 h3', { delay: 100, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.section5 p', { delay: 200, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
+    ScrollReveal().reveal('.section5 img', { delay: 500, distance: '10px', origin: 'bottom', easing: 'ease-in-out' });
 
 
     $('.get_link').on('click', function (e) {
